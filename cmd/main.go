0249fb2c0c1f48a698baf7a286bbbb00e855dd80
SmartWalle <smartwalle@gmail.com>
2021-08-28 09:35:38 +0800

    init

---
+++ cmd/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"github.com/smartwalle/loop4go"
+	"time"
+)
+
+func main() {
+	var queue = loop4go.NewEventQueue()
+
+	var loop *loop4go.Loop
+	var count = 0
+	loop = loop4go.NewLoop(time.Second, queue, func() {
+		count++
+		fmt.Println(time.Now(), count)
+
+		if count > 2 {
+			queue.Stop()
+		}
+	})
+	loop.Start()
+
+	queue.Start()
+	queue.Wait()
+}
